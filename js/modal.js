9f2f5aed69a2d0bbbc0d422dee8edd35f31eb083
enemisan <oritsemisanenemigin@gmail.com>
2023-06-26 20:31:48 +0100

    changes

--- js/modal.js
+++ js/modal.js
@@ -12,14 +12,25 @@ for (let i = 0; i < ap_card.length; i++) {
     })
 }
 
-for (let i = 0; i < sub_box.length; i++) {
-    sub_box[i].addEventListener('click', () => {
-        modal.classList.add('modal-show');
-        modal_wrap.classList.add('modal-wrap-show');
-    })
-}
-
 modal_close.addEventListener('click', () => {
     modal.classList.remove('modal-show');
     modal_wrap.classList.remove('modal-wrap-show');
+})
+
+
+// Events Modal
+const events_modal = document.getElementById("events-modal-ctn");
+const events_modal_wrap = document.getElementById("events-modal-wrap");
+const events_modal_close = document.getElementById("events-modal-close");
+
+for (let i = 0; i < sub_box.length; i++) {
+    sub_box[i].addEventListener('click', () => {
+        events_modal.classList.add('modal-show');
+        events_modal_wrap.classList.add('modal-wrap-show');
+    })
+}
+
+events_modal_close.addEventListener('click', () => {
+    events_modal.classList.remove('modal-show');
+    events_modal_wrap.classList.remove('modal-wrap-show');
 })
